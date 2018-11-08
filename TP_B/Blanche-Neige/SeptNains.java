7e6d64836ef1ebefa7992c44e0409026fff2c214
tiramisu <younesrouabhia@gmail.com>
2018-11-08 23:35:20 +0100

    Version finale

--- TP_B/Blanche-Neige/SeptNains.java
+++ TP_B/Blanche-Neige/SeptNains.java
@@ -6,13 +6,15 @@ import javax.print.event.PrintJobListener;
 
 public class SeptNains {
     public static void main(String[] args) {
-        int nbNains = 7;
-        String nom [] = {"Simplet", "Dormeur",  "Atchoum", "Joyeux", "Grincheux", "Prof", "Timide"};
+        final int nbNains = 7;
+        final String nom [] = { "Simplet", "Dormeur",  "Atchoum", 
+                                "Joyeux", "Grincheux", "Prof", "Timide"};
         Nain nain [] = new Nain [nbNains];
         for(int i = 0; i < nbNains; i++) nain[i] = new Nain(nom[i]);
         for(int i = 0; i < nbNains; i++) nain[i].start();
 
-        try { Thread.sleep(2000); } catch (InterruptedException e) {}
+        // Interrompre les nains au bout de 5 secondes
+        try { Thread.sleep(5000); } catch (InterruptedException e) {}
         for(int i = 0; i < nbNains; i++) {	
             nain[i].interrupt();
         }
@@ -24,22 +26,36 @@ public class SeptNains {
 }    
 
 class BlancheNeige {
-    private volatile boolean libre = true;        // Initialement, Blanche-Neige est libre.
+    private volatile boolean libre = true; // Initialement, Blanche-Neige est libre.
     private volatile ArrayList<String> fileDAttente = new ArrayList<>();
 
     public synchronized void requerir() {
         System.out.println("\t" + Thread.currentThread().getName()
                            + " veut un accès exclusif à la ressource");
         fileDAttente.add(Thread.currentThread().getName());
+        
+        // System.out.print("Contenu de la file d'attente : - ");
+        // for (String nain : fileDAttente) {
+        //     System.out.print(nain + "-");
+        // }
+        // System.out.println();
     }
 
-    public synchronized void acceder() {
-        while(Thread.currentThread().getName().compareTo(fileDAttente.get(0)) != 0 || !libre) { // Le nain s'endort sur l'objet bn
-            try { wait(); } catch (InterruptedException e) { Thread.currentThread().interrupt(); break; }
+    public synchronized boolean acceder() {
+        String threadName = Thread.currentThread().getName();
+        
+        while(!libre || threadName.compareTo(fileDAttente.get(0)) != 0) {
+            try { wait(); } // Le nain s'endort sur l'objet bn
+            catch (InterruptedException e) { 
+                Thread.currentThread().interrupt(); 
+                return false; 
+            }
         }
+
         libre = false;
-        System.out.println("\t\t" + Thread.currentThread().getName()
+        System.out.println("\t\t" + threadName
                            + " accède à la ressource.");
+        return true;
     }
 
     public synchronized void relacher() {
@@ -59,50 +75,14 @@ class Nain extends Thread {
     public void run() {
         while(!this.isInterrupted()) {
             bn.requerir();
-            bn.acceder();
-            System.out.println("\t\t" + getName() + " a un accès exclusif à Blanche-Neige.");
-            try {sleep(1000);} catch (InterruptedException e) { this.interrupt(); }
-            bn.relacher();
+            if (bn.acceder() == true) {
+                System.out.println("\t\t" + getName() 
+                        + " a un accès exclusif à Blanche-Neige.");
+                try { sleep(1000); }
+                catch (InterruptedException e) { this.interrupt(); }
+                finally { bn.relacher(); }
+            }
         }
         System.out.println(getName() + " a terminé!");
     }	
 }
-
-/*
-  $ make
-  $ java SeptNains
-  Simplet veut un accès exclusif à la ressource
-  Grincheux veut un accès exclusif à la ressource
-      Grincheux accède à la ressource.
-  Prof veut un accès exclusif à la ressource
-          Grincheux a un accès exclusif à Blanche-Neige.
-  Joyeux veut un accès exclusif à la ressource
-  Atchoum veut un accès exclusif à la ressource
-  Dormeur veut un accès exclusif à la ressource
-  Timide veut un accès exclusif à la ressource
-               Grincheux relâche la ressource.
-  Grincheux veut un accès exclusif à la ressource
-       Grincheux accède à la ressource.
-           Grincheux a un accès exclusif à Blanche-Neige.
-		       Grincheux relâche la ressource.
-  Grincheux veut un accès exclusif à la ressource
-      Grincheux accède à la ressource.
-          Grincheux a un accès exclusif à Blanche-Neige.
-              Grincheux relâche la ressource.
-  Grincheux veut un accès exclusif à la ressource
-       Grincheux accède à la ressource.
-           Grincheux a un accès exclusif à Blanche-Neige.
-		       Grincheux relâche la ressource.
-  Grincheux veut un accès exclusif à la ressource
-      Grincheux accède à la ressource.
-          Grincheux a un accès exclusif à Blanche-Neige.
-              Grincheux relâche la ressource.
-  Grincheux veut un accès exclusif à la ressource
-       Grincheux accède à la ressource.
-           Grincheux a un accès exclusif à Blanche-Neige.
-		       Grincheux relâche la ressource.
-  Grincheux veut un accès exclusif à la ressource
-      Grincheux accède à la ressource.
-          Grincheux a un accès exclusif à Blanche-Neige.
-              Grincheux relâche la ressource.
-*/
